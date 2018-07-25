cd131f467f4bd1a7ef3b150bc9e74b84e48234a4
KevinWJohnson <kevin.johnson3@cox.net>
2018-07-24 21:55:08 -0700

    Initial files and directory structure

---
+++ db/burgers_db.sql
@@ -0,0 +1,19 @@
+### Schema
+
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+	id int NOT NULL AUTO_INCREMENT,
+	burger_name varchar(255) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+);
+
+INSERT INTO burgers (burger_name) VALUES ('Double Bacon Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('BBQ Burger');
+INSERT INTO burgers (burger_name) VALUES ('Hawaiian Burger');
+
+SELECT * FROM burgers;
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Double Bacon Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('BBQ Burger');
+INSERT INTO burgers (burger_name) VALUES ('Hawaiian Burger');
+
+SELECT * FROM burgers;
